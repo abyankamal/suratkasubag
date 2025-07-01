f63f9890100d1207940af19fba319d22608aeda6
Asus Juara <abyankamal8@gmail.com>
2025-07-01 10:35:23 +0700

    change layout footer and login

--- resources/js/layouts/login/login-footer.tsx
+++ resources/js/layouts/login/login-footer.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 export default function LoginFooter() {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="w-full py-4 text-center text-sm text-muted-foreground bg-white">
       <div className="container mx-auto px-4">
